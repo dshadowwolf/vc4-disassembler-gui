676cd94bc8446a5a8f79a5232430af6788ce72af
Daniel Hazelton <dshadowwolf@gmail.com>
2019-02-12 18:58:28 -0500

    fix things up, get the logging working

--- ui/js/runner.js
+++ ui/js/runner.js
@@ -1,4 +1,41 @@
+class Logger {
+    constructor() {
+    }
+
+    info(text) {	
+	this.logInner("INFO", text);
+    }
+
+    debug(text) {
+	this.logInner("DEBUG", text);
+    }
+
+    warn(text) {
+	this.logInner("WARNING", text);
+    }
+
+    error(text) {
+	this.logInner("ERROR", text);
+    }
+
+    fatal(text) {
+	this.logInner("FATAL", text);
+    }
+
+    logInner(prefix, text) {
+	$(this._sel).append("<br>"+prefix+": "+text);
+    }
+
+    setSelector(selector) {
+	this._sel = selector;
+    }
+}
+
+const log = new Logger();
+
 $(document).ready(function() {
+    log.setSelector("#log-window div#log");
+    $("div#log").append("VC4 Tracing Disassembler v1.0.0-alpha1<br>Copyright &copy;2019 Daniel \"DShadowWolf\" Hazelton and others<br>Using Node.JS and Electron<br>Starting up now...<br>");
     let k = [];
     for(let i=0; i <= 4096; i += 32) {
 	let h = "0x";
@@ -12,6 +49,8 @@ $(document).ready(function() {
     $("#memory-view").css("height", $("#register-view").height());
     $("#stack-view").css("height", $("#register-view").height());
     $("#memory-view").css("width", ($("body").outerWidth() - $("#register-view").width() - $("#stack-view").width() - 20).toString()+"px");
+    $("#log-wndow").css("width", $("body").outerWidth());
+    $("#log-window").css("height", ($(window).height() - $("#register-view").height()-20).toString()+"px");
     let n = [];
     for(let j = 0; j <= 256; j += 4)
 	n.push("0x" + j.toString(16).padStart(8,'0') + ": 0x00000000");
@@ -23,5 +62,8 @@ $(window).resize(function() {
     $("#memory-view").css("height", $("#register-view").height());
     $("#stack-view").css("height", $("#register-view").height());
     $("#memory-view").css("width", ($("body").outerWidth() - $("#register-view").width() - $("#stack-view").width() - 20).toString()+"px");
-    console.log("current mem-view: "+$("#memory-view").width()+"x"+$("#memory-view").height());
+    $("#log-wndow").css("width", $("body").outerWidth());
+    $("#log-window").css("height", ($(window).height() - $("#register-view").height()-20).toString()+"px");
+    log.debug("current mem-view: "+$("#memory-view").width()+"x"+$("#memory-view").height());
+    log.debug("log window is: "+$("#log-window").width()+"x"+$("#log-window").height());
 });
