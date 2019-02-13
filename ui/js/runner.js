5aa7b4df301c83224ad875fbae84973fb9b8fab1
Daniel Hazelton <dshadowwolf@gmail.com>
2019-02-12 19:50:40 -0500

    style work, instruction stream view, other bits...

--- ui/js/runner.js
+++ ui/js/runner.js
@@ -41,16 +41,22 @@ $(document).ready(function() {
 	let h = "0x";
 	let b = i.toString(16);
 	h += b.padStart(8,'0');
-	h += ": 00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000";
+	h += ": <span class=\"mem-line\">00000000 00000000 00000000 00000000 00000000 00000000 00000000 00000000</span>  <span class=\"chars\">................................</span>";
 	k.push(h);
     }
     let fin = k.join("<br>");
     $("#memory-view").html(fin);
     $("#memory-view").css("height", $("#register-view").height());
     $("#stack-view").css("height", $("#register-view").height());
-    $("#memory-view").css("width", ($("body").outerWidth() - $("#register-view").width() - $("#stack-view").width() - 20).toString()+"px");
+    $("#instruction-view").css("height", $("#register-view").height());
+    $("#memory-view").css("width", ($("body").outerWidth() - $("#register-view").width() - $("#stack-view").width() - $("#instruction-view").width() - 26).toString()+"px");
     $("#log-wndow").css("width", $("body").outerWidth());
-    $("#log-window").css("height", ($(window).height() - $("#register-view").height()-20).toString()+"px");
+    $("#log-window").css("height", ($(window).height() - $("#register-view").height() - 6).toString()+"px");
+    let p = [];
+    for(let x = 0; x < 4096; x += 2)
+	p.push("0x"+x.toString(16).padStart(8,'0')+": bkpt");
+    $("#instruction-view").html(p.join("<br>"));
+    
     let n = [];
     for(let j = 0; j <= 256; j += 4)
 	n.push("0x" + j.toString(16).padStart(8,'0') + ": 0x00000000");
@@ -61,9 +67,10 @@ $(document).ready(function() {
 $(window).resize(function() {
     $("#memory-view").css("height", $("#register-view").height());
     $("#stack-view").css("height", $("#register-view").height());
-    $("#memory-view").css("width", ($("body").outerWidth() - $("#register-view").width() - $("#stack-view").width() - 20).toString()+"px");
-    $("#log-wndow").css("width", $("body").outerWidth());
-    $("#log-window").css("height", ($(window).height() - $("#register-view").height()-20).toString()+"px");
+    $("#memory-view").css("width", ($("body").outerWidth() - $("#register-view").width() - $("#stack-view").width() - $("#instruction-view").width()  - 26).toString()+"px");
+    $("#instruction-view").css("height", $("#register-view").height());
+    $("#log-wndow").css("width", $("#emul-view").width());
+    $("#log-window").css("height", ($(window).height() - $("#register-view").height() - 6).toString()+"px");
     log.debug("current mem-view: "+$("#memory-view").width()+"x"+$("#memory-view").height());
     log.debug("log window is: "+$("#log-window").width()+"x"+$("#log-window").height());
 });
